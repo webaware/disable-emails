0ab0255b45f8b62193c193cde07e7f505c84f7bf
Ross McKay <rmckay@webaware.com.au>
2016-11-21 16:43:51 +1100

    add tasks for creating .zip

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,41 @@
 module.exports = function (grunt) {
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON("package.json")
+		pkg: grunt.file.readJSON("package.json"),
+
+		clean: [ "dist/**" ],
+
+		copy: {
+			main: {
+				files: [
+					{
+						src: ["./**", "!./node_modules/**", "!./Gruntfile.js", "!./package.json"],
+						dest: "dist/<%= pkg.name %>/"
+					}
+				]
+			}
+		},
+
+		compress: {
+			options: {
+				archive: "./dist/<%= pkg.name %>-<%= pkg.version %>.zip",
+				mode: "zip"
+			},
+			all: {
+				files: [{
+					expand: true,
+					cwd: "./dist/",
+					src: [ "<%= pkg.name %>/**" ]
+				}]
+			}
+		}
 
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks("grunt-contrib-compress");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+
+	grunt.registerTask("release", ["clean","copy","compress"]);
 
 };
