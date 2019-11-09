199f815adb0c7db65f0a9247d25c5de170bec134
Ross McKay <rmckay@webaware.com.au>
2019-11-09 18:26:14 +1100

    update classes in "already defined" admin notice

--- views/warn-already-defined.php
+++ views/warn-already-defined.php
@@ -4,6 +4,6 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<div class="error">
+<div class="notice notice-error">
 	<p><?php esc_html_e('Emails are not disabled! Something else has already declared wp_mail(), so Disable Emails cannot stop emails being sent!', 'disable-emails'); ?></p>
 </div>
