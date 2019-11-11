7e473836db4158458aab20324d44299863861e10
Ross McKay <rmckay@webaware.com.au>
2019-11-11 21:53:19 +1100

    compile js

---
+++ js/settings.js
@@ -0,0 +1,37 @@
+"use strict";
+
+(function (settings) {
+  addHandler("disable-emails-mu-enable", "click", function () {
+    if (confirm(settings.msg.mu_activate)) {
+      mustUse('activate');
+    }
+  });
+  addHandler("disable-emails-mu-disable", "click", function () {
+    if (confirm(settings.msg.mu_deactivate)) {
+      mustUse('deactivate');
+    }
+  });
+  /**
+  * add an event handler to element, if element is found
+  * @param {String} selector
+  * @param {String} event
+  * @param {Function} handler
+  */
+
+  function addHandler(selector, event, handler) {
+    var element = document.getElementById(selector);
+
+    if (element) {
+      element.addEventListener(event, handler, false);
+    }
+  }
+  /**
+  * reload page with request to enable / disable the must-use plugin
+  * @param {String} action
+  */
+
+
+  function mustUse(action) {
+    document.location = settings.mu_url + "&action=" + action;
+  }
+})(disable_emails_settings);
